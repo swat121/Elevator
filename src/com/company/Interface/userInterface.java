1ff951760e1fdcdc7c32cdc4dc8082cb3167275c
Denis <stark2542@gmail.com>
2021-10-04 12:55:52 +0300

    Add: userInterface

---
+++ src/com/company/Interface/userInterface.java
@@ -0,0 +1,8 @@
+package com.company.Interface;
+
+public interface userInterface {
+    void getElevator();
+    void choiceFloorButton(int numberFloor);
+    void callDispatcher();
+    void stopButton();
+}
